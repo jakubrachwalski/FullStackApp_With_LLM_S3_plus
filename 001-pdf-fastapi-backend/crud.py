a6ef347c37c1aec29a27126cdf861da74919b642
jrachwal <kuba.rachwalski@gmail.com>
2025-07-20 23:21:37 +0900

    Third commit with FA v12

--- 001-pdf-fastapi-backend/crud.py
+++ 001-pdf-fastapi-backend/crud.py
@@ -21,16 +21,26 @@ def read_pdfs(db: Session, selected: bool = None):
 def read_pdf(db: Session, id: int):
     return db.query(models.PDF).filter(models.PDF.id == id).first()
 
-def update_pdf(db: Session, id: int, pdf: schemas.PDFRequest):
-    db_pdf = db.query(models.PDF).filter(models.PDF.id == id).first()
-    if db_pdf is None:
-        return None
-    update_data = pdf.dict(exclude_unset=True)
-    for key, value in update_data.items():
-        setattr(db_pdf, key, value)
-    db.commit()
-    db.refresh(db_pdf)
-    return db_pdf
+def upload_pdf(db: Session, file: UploadFile, file_name: str):
+    s3_client = Settings.get_s3_client()
+    BUCKET_NAME = Settings().AWS_S3_BUCKET
+    
+    try:
+        s3_client.upload_fileobj(
+            file.file,
+            BUCKET_NAME,
+            file_name,
+            ExtraArgs={'ACL': 'public-read'}  # <-- THIS LINE MAKES IT PUBLIC
+        )
+        file_url = f'https://{BUCKET_NAME}.s3.amazonaws.com/{file_name}'
+        
+        db_pdf = models.PDF(name=file.filename, selected=False, file=file_url)
+        db.add(db_pdf)
+        db.commit()
+        db.refresh(db_pdf)
+        return db_pdf
+    except NoCredentialsError:
+        raise HTTPException(status_code=500, detail="Error in AWS credentials")
 
 def delete_pdf(db: Session, id: int):
     db_pdf = db.query(models.PDF).filter(models.PDF.id == id).first()
